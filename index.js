3df49d41d098f6c740da717081abc581815fb613
Peter Yu <peter1.yu@cibc.com>
2016-12-28 21:08:26 -0500

    Added a simple React component that runs inside ExpressJS.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+var path = require('path');
 var express = require('express');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('It Works!');             // one line response
-  // res.sendfile('index.html');     // or send a webpage you designed
+app.use(express.static(path.join(__dirname, 'src/client/public')));
+
+app.get('*', function response(req, res) {
+  res.sendFile(path.join(__dirname, 'src/client/public/index.html'));
 });
 
 var server = app.listen(8080, function () {
-
   console.log('Node Express Webserver Started');
-
 });
---
+++ src/client/app/Application.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Application extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Wake up before you go go!!</h1>
+      </div>
+    )
+  }
+}
